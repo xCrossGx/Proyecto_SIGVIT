3ebe67185dd6744cde65dce7dc00ff12f4fc7ebe
OmarAZL <zabalaomaralfredo@gmail.com>
2026-02-15 10:57:38 -0400

    Agregar nuevas tablas y extensiones a la base de datos; implementar controlador y servicio para consultas.

---
+++ consultas/consultas.service.js
@@ -0,0 +1,9 @@
+const { customAlphabet } = require('nanoid')
+
+const nanoid = customAlphabet('0123456789ABCDEFGHJKLMNPQRSTUVWXYZ', 6);
+
+const createConsulta = (consulta) => {
+    id = nanoid()
+    
+}
+
--- database_manager.js
+++ database_manager.js
@@ -18,6 +18,20 @@ const getDB = () => {
 
 const crearTablas = async () => {
     try {
+
+        await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; CREATE EXTENSION IF NOT EXISTS "citext";`)
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS usuarios (
+                            uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                            username CITEXT NOT NULL,
+                            password_hash VARCHAR(255) NOT NULL,
+                            cedula VARCHAR(20) UNIQUE NOT NULL,
+                            nombre VARCHAR(100) NOT NULL,
+                            apellido VARCHAR(100) NOT NULL,
+                            fecha_registro timestamp DEFAULT CURRENT_TIMESTAMP
+                        );`
+                    )
+
         await pool.query(`CREATE TABLE IF NOT EXISTS pacientes (
                         cedula VARCHAR(20) PRIMARY KEY,
                         nombre VARCHAR(100) NOT NULL,
@@ -27,11 +41,14 @@ const crearTablas = async () => {
                         );`
                     )
         await pool.query(`CREATE TABLE IF NOT EXISTS consultas (
-                        cedula VARCHAR(20) NOT NULL,
+                        id VARCHAR(6) NOT NULL,
+                        cedula_paciente VARCHAR(20) NOT NULL,
+                        cedula_medico VARCHAR(20) NOT NULL,
                         fecha_consulta TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         descripcion TEXT NOT NULL,
-                        PRIMARY KEY (cedula, fecha_consulta),
-                        FOREIGN KEY (cedula) REFERENCES pacientes(cedula) ON DELETE CASCADE
+                        PRIMARY KEY (cedula_paciente, id),
+                        FOREIGN KEY (cedula_paciente) REFERENCES pacientes(cedula) ON DELETE CASCADE,
+                        FOREIGN KEY (cedula_medico) REFERENCES usuarios(cedula) ON DELETE CASCADE
                     );`
                 )
     } catch (error) {
