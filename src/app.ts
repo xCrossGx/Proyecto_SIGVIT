ca41d77d9604f698ec2e25acb5ec4418d76f0100
TH3KYAN <manueljesusrodriguez031@gmail.com>
2026-02-25 15:46:03 -0400

    feat: initialize Express server with CORS middleware and define API routes for patients, consultations, devices, and authentication.

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,20 @@ import { SERVER_PORT } from './utils/config.js';
 //import './services/dispositivos.service.js'
 
 import pacientesController from './controllers/pacientes.controller.js';
-//import dispositivosController from './controllers/dispositivos.controller.js';
+import dispositivosController from './controllers/dispositivos.controller.js';
 import authController from './auth/auth.controller.js'
 import consultasController from './controllers/consultas.controller.js';
 
 const app = express();
 
 // 2. Configura CORS antes de cualquier ruta
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
 // Tus rutas actuales
 app.use('/pacientes', pacientesController);
 app.use('/consultas', consultasController);
-//app.use('/dispositivos', dispositivosController);
+app.use('/dispositivos', dispositivosController);
 app.use('/auth', authController);
 
 app.listen(SERVER_PORT, () => {
